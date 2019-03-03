359942f5b322b9fad9b38d8f5c0ef0ed7ae3a884
Geoffry Song <goffrie@gmail.com>
2019-03-03 13:08:46 -0800

    verify harder

--- src/index.ts
+++ src/index.ts
@@ -15,18 +15,15 @@ async function main(): Promise<void> {
     const db: storage.Storage = await storage.createStorage(process.env.DB || ':memory:');
     const slackClient = new WebClient(OAUTH_TOKEN);
     const postMutex: Mutex = new Mutex();
-    const verify = verifier(SIGNING_SECRET);
 
     // for parsing application/x-www-form-urlencoded - sent to slash commands/button clicks
-    // app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({
+        extended: true,
+        verify: verifier(SIGNING_SECRET),
+    }));
 
     app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 
-    // XXX: megajank
-    app.use('/create', verify);
-    app.use('/vote', verify);
-
-    app.use(bodyParser.urlencoded({extended: true}));
     app.post('/create', async (req: Request, res: Response) => {
         // req has: channel_id, user_id, text (full command text)
         console.log(req.body);
--- src/verify.ts
+++ src/verify.ts
@@ -1,31 +1,26 @@
 import {createHmac, timingSafeEqual} from "crypto";
-import {Request, Response} from "express";
+import {IncomingMessage, ServerResponse} from "http";
 
-export function verifier(secret: string): (request: Request, response: Response, next: () => void) => void {
-    return (request: Request, response: Response, next: () => void): void => {
+export class ValidationError extends Error {}
+
+export function verifier(secret: string): (request: IncomingMessage, response: ServerResponse, body: Buffer, encoding: string) => void {
+    return (request: IncomingMessage, _response: ServerResponse, body: Buffer, _encoding: string): void => {
         const request_ts = request.headers['x-slack-request-timestamp'];
         const signature_header = request.headers['x-slack-signature'];
         if (typeof request_ts != 'string' || typeof signature_header != 'string') {
-            // there must only be one
-            response.sendStatus(400);
-            return;
+            throw new ValidationError("Bad headers");
         }
         if (Math.abs(Date.now() / 1000 - (+request_ts)) > 60 * 5) {
             // timestamp too far
-            response.sendStatus(400);
-            return;
+            throw new ValidationError("Bad timestamp");
         }
-        const request_body = request.body;
-        const message = `v0:${request_ts}:${request_body}`;
         const hmac = createHmac('sha256', secret);
-        hmac.update(message);
+        hmac.update(`v0:${request_ts}:`);
+        hmac.update(body);
         const required_signature = Buffer.from('v0=' + hmac.digest('hex'));
         const provided_signature = Buffer.from(signature_header);
-        if (timingSafeEqual(required_signature, provided_signature)) {
-            // good.
-            next();
-        } else {
-            response.sendStatus(400);
+        if (!timingSafeEqual(required_signature, provided_signature)) {
+            throw new ValidationError("Bad signature");
         }
     }
 }
