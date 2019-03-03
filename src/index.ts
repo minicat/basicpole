ad66100adabef9a1e08f106a1231acf79ea2f531
Geoffry Song <goffrie@gmail.com>
2019-03-03 00:53:49 -0800

    do votes

--- src/index.ts
+++ src/index.ts
@@ -38,13 +38,23 @@ async function main(): Promise<void> {
         res.send('you hit create!')
     })
 
-    app.post('/vote', (req: Request, res: Response) => {
+    app.post('/vote', async (req: Request, res: Response) => {
         // req has `payload` parameter (parse as json) that corresponds to the button clicked
         // of note: payload.user.id, payload.actions[0].action_id, payload.container.message_ts, payload.container.channel_id
         // see https://api.slack.com/messaging/interactivity/enabling pt 3
         const payload = JSON.parse(req.body.payload);
-        console.log(payload);
-        res.send('you hit vote!');
+        const user: string = payload.user.id;
+        const option_id: number = parseInt(payload.actions[0].action_id);
+        const ts: string = payload.container.message_ts;
+        const channel_id: string = payload.container.channel_id;
+
+        try {
+            await db.vote(channel_id, ts, user, option_id);
+            res.send('');
+        } catch (e) {
+            console.error(e);
+            res.sendStatus(500);
+        }
     })
 
     app.listen(port, () => console.log(`BASIC POLE listening on port ${port}!`))
